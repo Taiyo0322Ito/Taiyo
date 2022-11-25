d3899a8c3c7376bc69db940847c9e6b40f58e8bc
Taiyo0322Ito <taiyo0322@ezweb.ne.jp>
2022-11-25 15:44:40 +0900

    不明

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -10,19 +11,11 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      *
      * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+     */ 
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+        
+        
     }
 }
